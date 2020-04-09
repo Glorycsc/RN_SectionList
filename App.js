2b1b77aad8869d3306d0a78718b36dd35b45f851
shicheng.chen <shicheng.chen@mobiliya.com>
2020-04-09 11:38:39 +0800

    test code with RN SectionList

---
+++ App.js
@@ -0,0 +1,126 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    SectionList
+} from 'react-native';
+
+import {
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+    const overrideRenderItem = ({ item, index, section: { title, data } }) => <Text key={index}>Override{item}</Text>
+
+    return (
+        <>
+            <StatusBar barStyle="dark-content"/>
+            <SafeAreaView>
+                <ScrollView
+                    contentInsetAdjustmentBehavior="automatic"
+                    style={styles.scrollView}>
+                    <Header/>
+                    <View style={styles.body}>
+                        <SectionList
+                            renderItem={({item, index, section}) => <Text key={index}>{item}</Text>}
+                            renderSectionHeader={({section: {title}}) => (
+                                <Text style={{fontWeight: "bold"}}>{title}</Text>
+                            )}
+                            sections={[
+                                {title: "Title1", data: ["item1", "item2"]},
+                                {title: "Title2", data: ["item3", "item4"]},
+                                {title: "Title3", data: ["item5", "item6"]}
+                            ]}
+                            keyExtractor={(item, index) => item + index}
+                        />
+                        <View style={styles.sectionContainer}>
+                            <Text style={styles.sectionTitle}>See Your Changes</Text>
+                            <Text style={styles.sectionDescription}>
+                                <ReloadInstructions/>
+                            </Text>
+                        </View>
+
+                        <SectionList
+                            renderItem={({ item, index, section }) => <Text key={index}>{item}</Text>}
+                            sections={[
+                                { title: 'Title1', data: ['item1', 'item2'], renderItem: overrideRenderItem },
+                                { title: 'Title2', data: ['item3', 'item4'] },
+                                { title: 'Title3', data: ['item5', 'item6'] },
+                            ]}
+                        />
+                        <View style={styles.sectionContainer}>
+                            <Text style={styles.sectionTitle}>Debug</Text>
+                            <Text style={styles.sectionDescription}>
+                                <DebugInstructions/>
+                            </Text>
+                        </View>
+                        <View style={styles.sectionContainer}>
+                            <Text style={styles.sectionTitle}>Learn More</Text>
+                            <Text style={styles.sectionDescription}>
+                                Read the docs to discover what to do next:
+                            </Text>
+                        </View>
+                        <LearnMoreLinks/>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    scrollView: {
+        backgroundColor: Colors.lighter,
+    },
+    engine: {
+        position: 'absolute',
+        right: 0,
+    },
+    body: {
+        backgroundColor: Colors.white,
+    },
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+        color: Colors.black,
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+        color: Colors.dark,
+    },
+    highlight: {
+        fontWeight: '700',
+    },
+    footer: {
+        color: Colors.dark,
+        fontSize: 12,
+        fontWeight: '600',
+        padding: 4,
+        paddingRight: 12,
+        textAlign: 'right',
+    },
+});
+
+export default App;
